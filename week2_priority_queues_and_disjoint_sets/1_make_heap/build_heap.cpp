67f49f1f5148934167dfcbdc43b3b6b2f0a7cf0b
Parag Kumar Goyal <1997parag.goyal@gmail.com>
2023-12-24 17:41:10 +0530

    added week2 solutions and priority queue reference

--- week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
+++ week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
@@ -2,60 +2,87 @@
 #include <vector>
 #include <algorithm>
 
-using std::vector;
 using std::cin;
 using std::cout;
-using std::swap;
-using std::pair;
 using std::make_pair;
+using std::pair;
+using std::swap;
+using std::vector;
 
-class HeapBuilder {
- private:
+class HeapBuilder
+{
+private:
   vector<int> data_;
-  vector< pair<int, int> > swaps_;
+  vector<pair<int, int>> swaps_;
 
-  void WriteResponse() const {
+  void WriteResponse() const
+  {
     cout << swaps_.size() << "\n";
-    for (int i = 0; i < swaps_.size(); ++i) {
+    for (int i = 0; i < swaps_.size(); ++i)
+    {
       cout << swaps_[i].first << " " << swaps_[i].second << "\n";
     }
   }
 
-  void ReadData() {
+  void ReadData()
+  {
     int n;
     cin >> n;
     data_.resize(n);
-    for(int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
       cin >> data_[i];
   }
 
-  void GenerateSwaps() {
+  void siftDown(int index, int n)
+  {
+    int minimum = index;
+    int left = 2 * index + 1;
+    int right = 2 * index + 2;
+    if (left < n && data_[left] < data_[minimum])
+    {
+      minimum = left;
+    }
+    if (right < n && data_[right] < data_[minimum])
+    {
+      minimum = right;
+    }
+
+    if (minimum != index)
+    {
+      swaps_.push_back({index, minimum});
+      swap(data_[index], data_[minimum]);
+      siftDown(minimum, n);
+    }
+  }
+
+  void GenerateSwaps()
+  {
     swaps_.clear();
-    // The following naive implementation just sorts 
+    // The following naive implementation just sorts
     // the given sequence using selection sort algorithm
     // and saves the resulting sequence of swaps.
-    // This turns the given array into a heap, 
+    // This turns the given array into a heap,
     // but in the worst case gives a quadratic number of swaps.
     //
     // TODO: replace by a more efficient implementation
-    for (int i = 0; i < data_.size(); ++i)
-      for (int j = i + 1; j < data_.size(); ++j) {
-        if (data_[i] > data_[j]) {
-          swap(data_[i], data_[j]);
-          swaps_.push_back(make_pair(i, j));
-        }
-      }
+    size_t size = data_.size();
+    for (int i = size / 2; i >= 0; --i)
+    {
+      siftDown(i, size);
+    }
   }
 
- public:
-  void Solve() {
+public:
+  void Solve()
+  {
     ReadData();
     GenerateSwaps();
     WriteResponse();
   }
 };
 
-int main() {
+int main()
+{
   std::ios_base::sync_with_stdio(false);
   HeapBuilder heap_builder;
   heap_builder.Solve();
--- week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.cpp
+++ week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.cpp
@@ -1,60 +1,97 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
-using std::vector;
 using std::cin;
 using std::cout;
+using std::pair;
+using std::priority_queue;
+using std::vector;
 
-class JobQueue {
- private:
+class Job
+{
+public:
+  int worker;
+  long long finishTime;
+  Job(int worker, long long finishTime)
+  {
+    this->worker = worker;
+    this->finishTime = finishTime;
+  }
+};
+
+class CompareJobs
+{
+public:
+  bool operator()(Job *job1, Job *job2)
+  {
+
+    return job1->finishTime == job2->finishTime ? job1->worker > job2->worker : job1->finishTime > job2->finishTime;
+  }
+};
+
+class JobQueue
+{
+private:
   int num_workers_;
   vector<int> jobs_;
 
   vector<int> assigned_workers_;
   vector<long long> start_times_;
 
-  void WriteResponse() const {
-    for (int i = 0; i < jobs_.size(); ++i) {
+  void WriteResponse() const
+  {
+    for (int i = 0; i < jobs_.size(); ++i)
+    {
       cout << assigned_workers_[i] << " " << start_times_[i] << "\n";
     }
   }
 
-  void ReadData() {
+  void ReadData()
+  {
     int m;
     cin >> num_workers_ >> m;
+    assigned_workers_.resize(m);
+    start_times_.resize(m);
     jobs_.resize(m);
-    for(int i = 0; i < m; ++i)
+    for (int i = 0; i < m; ++i)
       cin >> jobs_[i];
   }
 
-  void AssignJobs() {
+  void AssignJobs()
+  {
     // TODO: replace this code with a faster algorithm.
-    assigned_workers_.resize(jobs_.size());
-    start_times_.resize(jobs_.size());
-    vector<long long> next_free_time(num_workers_, 0);
-    for (int i = 0; i < jobs_.size(); ++i) {
-      int duration = jobs_[i];
-      int next_worker = 0;
-      for (int j = 0; j < num_workers_; ++j) {
-        if (next_free_time[j] < next_free_time[next_worker])
-          next_worker = j;
-      }
-      assigned_workers_[i] = next_worker;
-      start_times_[i] = next_free_time[next_worker];
-      next_free_time[next_worker] += duration;
+    priority_queue<Job *, vector<Job *>, CompareJobs> finishedJobs;
+
+    for (int i = 0; i < std::min(num_workers_, (int)jobs_.size()); ++i)
+    {
+      finishedJobs.push(new Job(i, (long long)jobs_[i]));
+      assigned_workers_[i] = i;
+      start_times_[i] = 0;
+    }
+
+    for (int i = num_workers_; i < (int)jobs_.size(); ++i)
+    {
+      Job *finishedJob = finishedJobs.top();
+      finishedJobs.pop();
+      start_times_[i] = finishedJob->finishTime;
+      assigned_workers_[i] = finishedJob->worker;
+      finishedJobs.push(new Job(finishedJob->worker, 0LL + finishedJob->finishTime + jobs_[i]));
     }
   }
 
- public:
-  void Solve() {
+public:
+  void Solve()
+  {
     ReadData();
     AssignJobs();
     WriteResponse();
   }
 };
 
-int main() {
+int main()
+{
   std::ios_base::sync_with_stdio(false);
   JobQueue job_queue;
   job_queue.Solve();
